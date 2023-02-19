066bcd8064121e05f1e3eed42842cd06cf02ec38
eger001 <mainforphp@gmail.com>
2023-02-19 23:54:49 +0200

    install auth forms, changed start page to /login, install bootstrap, node.js, nmp, scss and jquery

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,13 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles(['node_modules/bootstrap/dist/css/bootstrap.min.css'], 'public/css/bootstrap.css')
+    .scripts(['node_modules/jquery/dist/jquery.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'], 'public/js/bootstrap.js')
+    .sourceMaps();
